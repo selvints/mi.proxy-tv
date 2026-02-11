5dd2ed3fb1770f19eb7312cc940c8258fb6232fa
selvints <140195975+selvints@users.noreply.github.com>
2026-02-11 11:59:30 -0600

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.get('/stream/:canalId', async (req, res) => {
     const { canalId } = req.params;
     // Base de tu IPTV con tus credenciales
-    const IPTV_BASE = 'http://vipketseyket.top:8080/live';
+    const IPTV_BASE = 'http://vipketseyket.top:8080/live/VIP013911761680146102/77b83cecc0c6';
     const targetUrl = `${IPTV_BASE}/${canalId}`;
 
     try {
@@ -55,8 +55,6 @@ app.get('/stream/:canalId', async (req, res) => {
 });
 
 app.listen(port, () => console.log(`Proxy corriendo en puerto ${port}`));
-
-
 
 
 
